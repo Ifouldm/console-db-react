116cc2d2c1e5ddc84e35b3cf3e4b50456a504ab3
matt <matt@linux>
2021-01-25 15:27:04 +0000

    updates to format and styles of cards

--- src/components/ConsoleCard.js
+++ src/components/ConsoleCard.js
@@ -7,15 +7,17 @@ function ConsoleCard(props) {
     const { consoleData } = props;
     return (
         <Card className="m-3">
-            <Card.Img className="logoImg" variant="top" placeholder="https://via.placeholder.com/300" src={consoleData.logoUrl} alt="Console logo" />
-            <Card.Header>{consoleData.name}</Card.Header>
+            <Card.Header className="cardHeader">
+                <div>{consoleData.name}</div>
+                <Image className="logoImg" placeholder="https://via.placeholder.com/300" src={consoleData.logoUrl} alt="Console logo" />
+            </Card.Header>
             <Card.Body>
                 <Card.Text>{consoleData.description}</Card.Text>
                 <Image fluid placeholder="https://via.placeholder.com/300" src={consoleData.photoUrl} alt="Console Photo" />
                 <Card.Footer>
                     <IconButton icon={faEdit} link={`/console/update/${consoleData.id}`} variant="warning" />
                     {' '}
-                    <IconButton icon={faTrashAlt} link={`/console/update/${consoleData.id}`} variant="danger" />
+                    <IconButton icon={faTrashAlt} link={`/console/delete/${consoleData.id}`} variant="danger" />
                 </Card.Footer>
             </Card.Body>
         </Card>
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,6 +1,7 @@
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import IconButton from './IconButton';
 
 function GameCard(props) {
@@ -9,7 +10,59 @@ function GameCard(props) {
         <Card className="m-3">
             <Card.Header>{game.gameName}</Card.Header>
             <Card.Body>
-                <Card.Text>{game.description}</Card.Text>
+                <Card.Text>
+                    {game.description}
+                </Card.Text>
+                <ListGroup>
+                    {game.manufacturer
+                    && (
+                        <ListGroup.Item>
+                            Manufacturer:
+                            {game.manufacturer}
+                        </ListGroup.Item>
+                    )}
+                    {game.developer
+                    && (
+                        <ListGroup.Item>
+                            Developer:
+                            {game.developer}
+                        </ListGroup.Item>
+                    )}
+                    {game.year
+                    && (
+                        <ListGroup.Item>
+                            Year:
+                            {game.year}
+                        </ListGroup.Item>
+                    )}
+                    {game.genre
+                    && (
+                        <ListGroup.Item>
+                            Genre(s):
+                            {game.genre}
+                        </ListGroup.Item>
+                    )}
+                    {game.score
+                    && (
+                        <ListGroup.Item>
+                            Score:
+                            {game.score}
+                        </ListGroup.Item>
+                    )}
+                    {game.players
+                    && (
+                        <ListGroup.Item>
+                            Players:
+                            {game.players}
+                        </ListGroup.Item>
+                    )}
+                    <ListGroup.Item action>
+                        <Link to="/api/console/consoleID">
+                            Console:
+                            {game.console}
+                        </Link>
+                    </ListGroup.Item>
+                </ListGroup>
                 <IconButton icon={faEdit} link={`/game/update/${game.gameId}`} variant="warning" />
                 {' '}
                 <IconButton icon={faTrashAlt} link={`/game/delete/${game.gameId}`} variant="danger" />
