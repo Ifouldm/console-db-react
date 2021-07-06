5678e4e2e1f9b40175714be0927832458dfd78cc
matt <matt@linux>
2021-07-06 19:06:40 +0100

    Conversion to typescript

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useFetch = (url: string) => {
+    const [, setData] = useState(null);
+    const [, setLoading] = useState(true);
+    const [, setError] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(url)
+            .then((res) => {
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then((dataIn) => {
+                setData(dataIn);
+                setLoading(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setLoading(false);
+                setError(err.message);
+            });
+    }, [url]);
+};
+
+export default useFetch;
